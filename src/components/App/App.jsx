e5e29a25c398d02a8b7cdc0f97a1fe2feb88179f
Hellcat232 <maxkuich1993@gmail.com>
2024-04-25 19:25:23 +0300

    err

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.module.css";
-import HomePage from "../../pages/HomePage/HomePage";
-import MoviesPage from "../../pages/MoviesPage/MoviesPage";
-import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage";
-import MovieCast from "../MovieCast/MovieCast";
-import MovieReviews from "../MovieReviews/MovieReviews";
-import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
-import Navigation from "../Navigation/Navigation";
+import { Suspense, lazy } from "react";
+import Loader from "../Loader/Loader";
+
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const MoviesPage = lazy(() => import("../../pages/MoviesPage/MoviesPage"));
+const MovieDetailsPage = lazy(() =>
+  import("../../pages/MovieDetailsPage/MovieDetailsPage")
+);
+const MovieCast = lazy(() => import("../MovieCast/MovieCast"));
+const MovieReviews = lazy(() => import("../MovieReviews/MovieReviews"));
+const NotFoundPage = lazy(() =>
+  import("../../pages/NotFoundPage/NotFoundPage")
+);
+const Navigation = lazy(() => import("../Navigation/Navigation"));
 
 const App = () => {
   return (
-    <>
+    <Suspense fallback={<Loader />}>
       <Navigation />
 
       <Routes>
@@ -22,7 +29,7 @@ const App = () => {
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </>
+    </Suspense>
   );
 };
 
